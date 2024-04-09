cec3abb4759a9224bdc650717e0efdba8a6fdf72
Adop <chdn786od@gmail.com>
2024-04-10 01:21:21 +0200

    loading styles added using notiflix

--- frontend/src/app/loading.tsx
+++ frontend/src/app/loading.tsx
@@ -1,7 +1,16 @@
-export default function Loading() {
-  return (
-    <div style={{ background: 'red', width: '100vw', height: '100vh' }}>
-      Loading...
-    </div>
-  );
-}
+'use client';
+
+import { Loading } from "notiflix";
+import { useEffect } from "react";
+
+export default function Loader() {
+  useEffect(() => {
+    Loading.circle('please wait', { backgroundColor: '#181818', svgColor: '#FE59C2' });
+
+    return () => {
+      Loading.remove(500)
+    }
+  }, []);
+  
+  return null;
+};
