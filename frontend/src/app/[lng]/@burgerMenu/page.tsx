b067ec6f2e59a9fe0bca173342ff4eed90fe0252
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2024-04-16 15:04:35 +0300

    Refactor burgerMenu page

--- frontend/src/app/[lng]/@burgerMenu/page.tsx
+++ frontend/src/app/[lng]/@burgerMenu/page.tsx
@@ -1,9 +1,6 @@
-import { BurgerMenu, Button, LanguageSwitcher, NavBar } from '@/app/components';
-import { transition } from '@/app/lib/constants';
+import { BurgerMenu } from '@/app/components';
 import { useTranslation } from '@/app/i18n';
 import { PageProps } from '@/app/lib/types';
-import { DisableScroll, Svg, cn, endpoints } from '@/app/lib/utils';
-import Link from 'next/link';
 
 export default async function Page({
   params: { lng },
@@ -11,7 +8,7 @@ export default async function Page({
 }: Readonly<PageProps>) {
   const { t } = await useTranslation(lng, 'header');
   const { t: navT } = await useTranslation(lng, 'navBar');
-  const showMobMenu: boolean = searchParams?.mobMenu ? true : false;
+  const showMobMenu: boolean = !!searchParams?.mobMenu;
 
   return <BurgerMenu showMobMenu={showMobMenu} t={t} navT={navT} lng={lng} />;
 }
