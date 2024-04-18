94576877585cc8cbc066a0f118fc5127e8dc3ff1
Adop <chdn786od@gmail.com>
2024-04-19 00:39:12 +0200

    fixes

--- frontend/src/app/error.tsx
+++ frontend/src/app/error.tsx
@@ -29,9 +29,9 @@ export default function ErrorPage({ error, statusCode }: Readonly<ErrorProps>) {
         <>
             <Header lng={lng as Language} t={headerT} navT={navT} />
             
-            <section className='wk_mt-[140px] tablet:wk_mt-[180px] wk_mx-auto wk_w-fit wk_text-center wk_flex wk_flex-col wk_items-center'>
+            <section className='wk_mt-[140px] tablet:wk_mt-[180px] desktop:wk_mt-[240px] wk_mx-auto wk_w-fit wk_text-center wk_flex wk_flex-col wk_items-center'>
                 <h1 className='wk_font-300 wk_leading-none wk_text-[120px] wk_text-th_accent dekstop:wk_text-[239px] wk_mb-[78px] tablet:wk_mb-[32px] desktop:wk_mb-[40px]'>{statusCode ? statusCode : 'Unexpected error'}</h1>
-                <p className='wk_font-400 wk_text-[20px] wk_text-gray_400 tablet:wk_text-[40px] wk_mb-[156px] tablet:wk_mb-[66px] desktop:wk_mb-[80px]'>
+                <p className='wk_font-400 wk_text-[20px] wk_text-gray_400 tablet:wk_text-[40px] wk_mb-[156px] tablet:wk_mb-[66px] desktop:wk_mb-[80px] wk_max-w-[1080px]'>
                     {errorT('errorDescription')}
                     <a className={cn('wk_underline wk_text-th_accent hover:wk_text-hotPink_500 active:wk_text-hotPink_600 focus:wk_text-hotPink_600', transition)} href="https://www.instagram.com/viksi_fitness" target='_blank'>instagram</a>
                 </p>
